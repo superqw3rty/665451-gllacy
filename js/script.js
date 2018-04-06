be2ec8658b6cea85590cfaace36511b9358638f3
superqw3rty <vgosudarevv@gmail.com>
2018-04-06 18:07:49 +0300

    проверка logo

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var link = document.querySelector(".feedback-btn");
+      var popup = document.querySelector(".feedback-form");
+      var close = popup.querySelector(".modal-close")
+      var overlay = document.querySelector(".overlay");
+      var form = popup.querySelector(".feedback-form-js");
+      var name = form.querySelector(".feedback-name-input");
+      var email = form.querySelector(".feedback-email-input");
+
+      link.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.add("feedback-form-open");
+        overlay.classList.add("overlay-show");
+        name.focus();
+      });
+
+      close.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.remove("feedback-form-open");
+        overlay.classList.remove("overlay-show");
+      });
+
+      window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          evt.preventDefault();
+          if (popup.classList.contains("feedback-form-open")) {
+          popup.classList.remove("feedback-form-open");
+          overlay.classList.remove("overlay-show");
+          }
+        }
+      });
